ba8ce8bca5d2c121e648114aa0f92dc351694425
Shubhank2002 <maheshwarishubhank2002@gmail.com>
2026-07-09 11:45:12 +0530

    initializing project

---
+++ Server/src/app.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+module.exports = app
